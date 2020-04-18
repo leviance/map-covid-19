45dbed1b5610b6f255dc76db460d29e97f141ed8
Anthony Levi <drdung1999@kali.hoangdungdev.com>
2020-04-18 06:27:38 -0400

    Updates

---
+++ precache-manifest.1e3f833e0ba3fe28e0f72c97355fe755.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db01eebaacef3a3cfcd12cf9931724b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cedf4d8fc510e0c20395",
+    "url": "/static/css/2.75edc52f.chunk.css"
+  },
+  {
+    "revision": "b98726b1790d549b6d27",
+    "url": "/static/css/main.cae3ae40.chunk.css"
+  },
+  {
+    "revision": "cedf4d8fc510e0c20395",
+    "url": "/static/js/2.2cb2ea29.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2cb2ea29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b98726b1790d549b6d27",
+    "url": "/static/js/main.737016e1.chunk.js"
+  },
+  {
+    "revision": "18087ab926c68425810e",
+    "url": "/static/js/runtime-main.bb5b6ffb.js"
+  }
+]);
